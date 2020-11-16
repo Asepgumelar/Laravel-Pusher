4c04ce5e2d758252f351ed5e1e65d8c4c8606eec
Asep Gumelar <48475205+Asepgumelar@users.noreply.github.com>
2020-11-16 14:03:57 +0700

    first commit

--- database/migrations/2020_11_09_064352_create_notifications_table.php
+++ database/migrations/2020_11_09_064352_create_notifications_table.php
@@ -15,6 +15,7 @@ class CreateNotificationsTable extends Migration
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->uuid('id')->primary()->unique();
+            $table->uuid('user_id');
             $table->string('type');
             $table->morphs('notifiable');
             $table->text('data');
